4ce6d471a369a24379935b1a8f8f9429aa1cf0b0
ziggor01 <ziggor01@gmail.com>
2024-10-21 22:53:39 +0300

    [DZ] add provider HCP Vault

--- ec2/versions.tf
+++ ec2/versions.tf
@@ -1,9 +1,15 @@
 terraform {
-  required_version = ">1.7"
+  required_version = ">=1.7"
   required_providers {
+    #Провайдер AWS
     aws = {
       source  = "hashicorp/aws"
-      version = ">=5.71.0"
+      version = ">=5.70.0"
+    }
+    #Провайдер HCP Vault
+    vault = {
+      source = "hashicorp/vault"
+      version = ">=4.3.0"
     }
   }
 }
